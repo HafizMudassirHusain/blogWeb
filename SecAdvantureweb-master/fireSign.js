a0ef3636055bef62382ee648fb44e4467bb3c04c
Hafiz Mudassir Husain <160108451+HafizMudassirHusain@users.noreply.github.com>
2024-09-21 14:44:40 +0500

    Update fireSign.js

--- SecAdvantureweb-master/fireSign.js
+++ SecAdvantureweb-master/fireSign.js
@@ -1,27 +1,19 @@
 
 
-//   const firebaseConfig = {
-//     apiKey: "AIzaSyCfIkAeB114pEodDfgiUnFJ7EIbouKRccA",
-//     authDomain: "blogbook-92b00.firebaseapp.com",
-//     projectId: "blogbook-92b00",
-//     storageBucket: "blogbook-92b00.appspot.com",
-//     messagingSenderId: "626996935537",
-//     appId: "1:626996935537:web:826e20a20d12c279df94d0"
-//   };
-
   
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js'
 import { getAuth,createUserWithEmailAndPassword ,signInWithEmailAndPassword, GoogleAuthProvider,signInWithPopup
  } from 'https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCfIkAeB114pEodDfgiUnFJ7EIbouKRccA",
-    authDomain: "blogbook-92b00.firebaseapp.com",
-    projectId: "blogbook-92b00",
-    storageBucket: "blogbook-92b00.appspot.com",
-    messagingSenderId: "626996935537",
-    appId: "1:626996935537:web:826e20a20d12c279df94d0"
-  };
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
   const app = initializeApp(firebaseConfig);
  const auth = getAuth(app);
 var getbtn = document.getElementById('subtn')
